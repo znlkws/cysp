4e30b31ab5543f8a88d313575ae50099c86d834d
znlkws <ttxx.bnb1@gmail.com>
2025-12-08 10:15:44 +0800

    update style

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div class="flex flex-col h-screen bg-gray-50">
+  <div class="flex flex-col h-screen bg-gray-50" style="height: 100vh;">
     <Header class="flex-shrink-0" />
-    <div class="flex flex-1 overflow-hidden ">
+    <div class="flex flex-1">
       <router-view class="min-h-full w-full" />
     </div>
   </div>
